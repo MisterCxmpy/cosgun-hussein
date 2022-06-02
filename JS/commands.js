e3746b93f87b4549f1c052c13068e3066ff3ce7c
MisterCxmpy <60076142+MisterCxmpy@users.noreply.github.com>
2022-06-02 15:48:00 +0100

    initialcommit

---
+++ JS/commands.js
@@ -0,0 +1,81 @@
+var github = "https://github.com/mistercxmpy/";
+var email = 'mailto:cosgunhussein@gmail.com';
+
+about = [
+  "<br>",
+  "Hello, I am Cosgun Hussein!",
+  "I'm a aspiring software engineer that has been coding for 5+ years.",
+  "My primary language of choice is Python as I find it the most enjoyable language due to its simple syntax.",
+  "Over the years I have tried to pick up a few different languages such as C#, Java and JavaScript,",
+  "although my understanding of these languages is very basic.",
+  "I enjoy making random and useless programs for fun, while also making some more serious ones too.",
+  "Hope you enjoy browsing the site!",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'github         <a href="' + github + '" target="_blank">github/mistercxmpy' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "⚠ PROJECT SECTION IS CURRENTLY UNDER DEVELOPMENT ⚠",
+  "<br>",
+  "Type the name of the language to views its projects:",
+  "<br>",
+  "Python",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Who am I?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           Display list of commands',
+  '<span class="command">email</span>          Contact me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">themes</span>         Change the theme',
+  '<span class="command">cv</span>             Download my CV',
+  "<br>",
+];
+
+banner = [
+    '&nbsp;██████╗ ██████╗ ███████╗ ██████╗ ██╗   ██╗███╗   ██╗    ██╗  ██╗██╗   ██╗███████╗███████╗███████╗██╗███╗   ██╗',
+    '██╔════╝██╔═══██╗██╔════╝██╔════╝ ██║   ██║████╗  ██║    ██║  ██║██║   ██║██╔════╝██╔════╝██╔════╝██║████╗  ██║',
+    '██║     ██║   ██║███████╗██║  ███╗██║   ██║██╔██╗ ██║    ███████║██║   ██║███████╗███████╗█████╗  ██║██╔██╗ ██║',
+    '██║     ██║   ██║╚════██║██║   ██║██║   ██║██║╚██╗██║    ██╔══██║██║   ██║╚════██║╚════██║██╔══╝  ██║██║╚██╗██║',
+    '╚██████╗╚██████╔╝███████║╚██████╔╝╚██████╔╝██║ ╚████║    ██║  ██║╚██████╔╝███████║███████║███████╗██║██║ ╚████║',
+    '&nbsp;╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝    ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝╚═╝  ╚═══╝',
+    '<span class="color2">Welcome to my portfolio.</span>',
+    "<span class=\"color2\">Type</span> <span class=\"command\">'help'</span><span class=\"color2\"> to see list of available commands.</span>",
+];
+
+themes = [
+    '<br>',
+    `<a href="#" onclick=changeStyle("default");>default</a>`,
+    `<a href="#" onclick=changeStyle("dark");>dark</a>`,
+    `<a href="#" onclick=changeStyle("watermelon");>watermelon</a>`,
+    `<a href="#" onclick=changeStyle("cyberspace");>cyberspace</a>`,
+    `<a href="#" onclick=changeStyle("terminal");>terminal</a>`,
+    '<br>',
+]
+
+cv = [
+  '<br>',
+  'pdf          <a href="data/cosgun hussein - cv.pdf" download>download cv</a>',
+  'docx         <a href="data/cosgun hussein - cv.docx" download>download cv</a>',
+  '<br>',
+]
+
+python = [
+  '<br>',
+  '<a href="https://github.com/MisterCxmpy/Python-Snipping-Tool" target="_blank">python snipping tool' + "</a>",
+  '<a href="https://github.com/MisterCxmpy/Python-Drawing-Tool" target="_blank">python drawing tool' + "</a>",
+  '<a href="https://github.com/MisterCxmpy/Python-YouTube-MP3-Downloader" target="_blank">python youtube to mp3' + "</a>",
+  '<br>',
+]
---
+++ JS/main.js
@@ -0,0 +1,170 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+var theme = localStorage.getItem("themes");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+document.getElementById("currentTheme").setAttribute("href", theme);
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("guest@mistercxmpy.github.io:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "about":
+      loopLines(about, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Opening mailto:<a href="mailto:cosgunhussein@gmail.com">cosgunhussein@gmail.com</a>...', "color2", 80);
+      newTab(email);
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "themes":
+      loopLines(themes, "color2 margin", 80);
+    break;
+    case "cv":
+      loopLines(cv, "color2 margin", 80);
+    break;
+    // socials
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    // languages
+    case "python":
+      loopLines(python, "color2 margin", 80);
+    break;
+    default:
+      addLine("<span class=\"inherit\">shell: command not found: ".concat(cmd,". Try <span class=\"command\">'help'</span> to get started.</span> "), "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style, index * time);
+  });
+}
+
+function changeStyle(sheet) {
+    document.getElementById("currentTheme").setAttribute("href", "themes/" + sheet + ".css");  
+    textarea.focus();
+
+    if (typeof(Storage) !== "undefined") {
+      localStorage.setItem("themes", document.getElementById("currentTheme").getAttribute("href"));
+    }
+}
